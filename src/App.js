f7615ec1a4d0de084737130287b78d5fca8858e8
datacreator001 <amirrorphone@gmail.com>
2020-10-29 12:42:54 -0400

    4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-;
+const url='https://www.boredapi.com/api/activity/'
+console.log(url);
 const App = () => {
+const [activity,setActivity]=useState('')
+
+  const newActivity= ()=>{
+ fetch(url)
+		.then((res) => res.json())
+		.then(resJson=> setActivity(resJson.activity)) 
+}
+  
+useEffect(() => {
+  newActivity()
+}, [])
+  
   return (
     <div class="box">
       <h2>Activities to do if you are bored</h2>
-      <p>Activity</p>
-      <button>New Activity</button>
+      <p>{activity}</p>
+      <button onClick={newActivity}>New Activity</button>
     </div>
   );
 };
