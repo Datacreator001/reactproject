e2c5599676ee252d78c2247f45a44a7f0a84add1
datacreator001 <amirrorphone@gmail.com>
2020-10-30 19:11:57 -0400

    6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Activity from './Activity';
 import Type from './Type';
 import Modal from './Modal';
-// import './App.css'
+import "./button.css"
 
 const App = () => {
  
@@ -16,16 +16,19 @@ const App = () => {
 			<h2>Activities to do if you are bored</h2>
 			<Activity />
       <Modal ref={modalRef}>
-        <h1>Modal Header</h1>
+        <h1>About this App</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dignissimos eius, voluptatum alias provident eum reiciendis maiores a nihil beatae iste at cum eligendi, temporibus quia ipsa maxime vel? Tempore!
-          Facilis, nisi nobis et dolores dolorem libero voluptas magni reiciendis deserunt sint dolorum perspiciatis quod consectetur, mollitia doloremque voluptatum at beatae eius ipsa cupiditate, nam tenetur quae? Repellat, in officia!
-          Velit ab eius doloribus unde, fugiat sunt repellat molestiae! Blanditiis totam perspiciatis sint dicta dignissimos eaque corrupti repellat enim, laudantium nihil illum ratione! Facere vel ab error illo rerum delectus?
+         Click on New Activity button to generate a activity to do if you are bored!!!
         </p>
       </Modal>
-			<button onClick={openModal}> Open Modal</button>
+      
+			<button className='.button' onClick={openModal}> Open Modal</button>
 		</div>
 	);
 };
 
 export default App;
+
+
+// some parts of my project I had got from  youtube!!
+// such as the modal part!
